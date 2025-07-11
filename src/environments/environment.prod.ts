f85fcf29c99efe9ecd9844f43cd5bc61afe31a44
Jeferson belmiro <jeferson.belmiro@gmail.com>
2025-07-11 10:39:25 -0300

    fix: prod remote url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
   remotes: {
-    store: 'https://jefersonbelmiro.github.io/bookstore/store/remoteEntry.js',
-    account: 'https://jefersonbelmiro.github.io/bookstore/account/remoteEntry.js',
-    catalog: 'https://jefersonbelmiro.github.io/bookstore/catalog/remoteEntry.js',
-    review: 'https://jefersonbelmiro.github.io/bookstore/review/remoteEntry.js',
+    store: 'https://book-store-mfe.github.io/store/remoteEntry.js',
+    account: 'https://book-store-mfe.github.io/account/remoteEntry.js',
+    catalog: 'https://book-store-mfe.github.io/catalog/remoteEntry.js',
+    review: 'https://book-store-mfe.github.io/review/remoteEntry.js',
   }
 };
